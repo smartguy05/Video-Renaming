d029cc928d6d484a069a697d3b9614dde52413c4
Anthony James <anthony@anthonytheprogrammer.com>
2020-12-29 13:34:09 -0700

    initial commit

---
+++ RenameVideoForPlex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RenameVideoForPlex
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // open using command line
+            // use current directory or directory supplied
+
+            // get all folders in directory
+
+            // for each folder find all video files and rename to same as folder
+
+            // do same for subtitle files, but don't touch language suffix
+        }
+    }
+}
